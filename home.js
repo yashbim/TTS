4e34e54a47ea131bb707230be9cfafa6d8a2faea
yashbim <ybimsara03@gmail.com>
2023-10-20 00:30:40 +0530

    modified and implemented json for services data and implemented for gallery in homepage

---
+++ home.js
@@ -0,0 +1,29 @@
+//Loading JSON data
+
+fetch("services.json")
+    .then(response => response.json())
+    .then(services => {
+        console.log(services)
+
+        // Access service from json for prodcut name
+        const productname1 = services.product1;
+        const productname2 = services.product2;
+        const productname3 = services.product3;
+
+        // Access service from json for product description
+        const service1 = services.product1;
+1        
+
+        // Insert into html product name
+        document.getElementById('product1name').textContent=productname1.name;
+        document.getElementById('product2name').textContent=productname2.name;
+        document.getElementById('product3name').textContent=productname3.name;
+
+        // Insert into html product description
+
+        document.getElementById('s1').textContent=service1.description;
+        document.getElementById('s2').textContent=service2.description;
+        document.getElementById('s3').textContent=service3.description;
+    })
+
+    .catch(error => console.error("Error loading data from json"));
