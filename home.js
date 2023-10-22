1a15fe40353074f2e111a80697e983f5b06b5019
yashbim <ybimsara03@gmail.com>
2023-10-23 00:28:12 +0530

    remaining changes

--- home.js
+++ home.js
@@ -14,31 +14,31 @@ fetch("home.json")
     })
 
 
-    fetch("services.json")
-    .then(response => response.json())
-    .then(services => {
-        console.log(services)
+fetch("services.json")
+.then(response => response.json())
+.then(services => {
+    console.log(services)
 
-        // Access service from json for prodcut name
-        const productname1 = services.product1;
-        const productname2 = services.product2;
-        const productname3 = services.product3;
+    // Access service from json for prodcut name
+    const productname1 = services.product1;
+    const productname2 = services.product2;
+    const productname3 = services.product3;
 
-        // Access service from json for product description
-        const service1 = services.product1;
-        const service2 = services.product2;
-        const service3 = services.product3;
+    // Access service from json for product description
+    const service1 = services.product1;
+    const service2 = services.product2;
+    const service3 = services.product3;
 
-        // Insert into html product name
-        document.getElementById('product1name').textContent=productname1.name;
-        document.getElementById('product2name').textContent=productname2.name;
-        document.getElementById('product3name').textContent=productname3.name;
+    // Insert into html product name
+    document.getElementById('product1name').textContent=productname1.name;
+    document.getElementById('product2name').textContent=productname2.name;
+    document.getElementById('product3name').textContent=productname3.name;
 
-        // Insert into html product description
+    // Insert into html product description
 
-        document.getElementById('s1').textContent=service1.description;
-        document.getElementById('s2').textContent=service2.description;
-        document.getElementById('s3').textContent=service3.description;
-    })
+    document.getElementById('s1').textContent=service1.description;
+    document.getElementById('s2').textContent=service2.description;
+    document.getElementById('s3').textContent=service3.description;
+})
 
-    .catch(error => console.error("Error loading data from json"));
+.catch(error => console.error("Error loading data from json"));
