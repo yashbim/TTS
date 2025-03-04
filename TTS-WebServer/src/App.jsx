327b1e976726372aaccaaa4b192b21f7907c5439
yashbim <ybimsara03@gmail.com>
2025-03-04 23:32:46 +0530

    Created new project using javascript for webapp in TTS-WebServer

---
+++ TTS-WebServer/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+const App = () => {
+  return (
+  <div>
+        <div>
+      <h1 className="text-5xl font-bold underline">
+        Total Textile Solutions
+      </h1>
+    </div>
+    <hr />
+    <div>
+    <h1 className="text-5xl font-bold italic">
+      Total Textile Solutions
+    </h1>
+  </div>
+  </div>
+  )
+}
+
+export default App;
