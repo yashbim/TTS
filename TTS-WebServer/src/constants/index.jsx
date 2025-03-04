8d6170a10defe2c10d0f8f7a763613adfa03c117
yashbim <ybimsara03@gmail.com>
2025-03-05 00:41:32 +0530

    Added navbar items to list at TTS-WebServer\src\constants\index.jsx

---
+++ TTS-WebServer/src/constants/index.jsx
@@ -0,0 +1,8 @@
+// Navigation bar items list
+
+export const navBar_Items = [
+    { label: "Home", href:"#"},
+    { label: "About Us", href:"#"},
+    { label: "Our Services", href:"#"},
+    { label: "Contact", href:"#"}
+];
