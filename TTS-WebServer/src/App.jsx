576b0b58d5d9b272b738920e71a5589b14e27407
yashbim <ybimsara03@gmail.com>
2025-03-05 08:56:37 +0530

    Implemented navbar as a component in TTS-WebServer\src\components\navbar.jsx

--- TTS-WebServer/src/App.jsx
+++ TTS-WebServer/src/App.jsx
@@ -1,18 +1,12 @@
-import logo_long from "./assets/logo_long.png";
+import React from "react";  
+import Navbar from "./components/navbar.jsx";
 
-import React from "react"
 
 const App = () => {
   return (
-    <nav className="sticky top-0 py-3 backdrop-blur-lg border-b border-neutral-700/80 " style={{backgroundColor:'#CDDC39'}}>
-      <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex items-center flex-shrink-0">
-            <img className="h-15 mr-2 border-1 rounded-md" src={logo_long} alt="logo" />
-            {/* <span className="text-xl tracking-tight">TTS</span> */}
-        </div> 
-      </div>
-    </nav>
-    
+    <>
+      <Navbar />
+    </>
   )
 }
 
--- TTS-WebServer/src/components/navbar.jsx
+++ TTS-WebServer/src/components/navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import logo_long from "../assets/logo_long.png";
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
+      <nav className="sticky top-0 py-3 backdrop-blur-lg border-b border-neutral-700/80 " style={{backgroundColor:'#CDDC39'}}>
+        <div className="container px-4 mx-auto relative text-sm">
+          <div className="flex items-center flex-shrink-0">
+              <img className="h-15 mr-2 border-1 rounded-md" src={logo_long} alt="logo" />
+              {/* <span className="text-xl tracking-tight">TTS</span> */}
+          </div> 
+        </div>
+      </nav>
   )
 }
 
-export default navbar
+export default Navbar
