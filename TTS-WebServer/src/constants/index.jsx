7ce64e44c97d44f1853e139630d97085df742f7f
yashbim <ybimsara03@gmail.com>
2025-07-07 12:12:33 +0530

    Added default next webapp

--- TTS-WebServer/src/constants/index.jsx
+++
@@ -1,8 +0,0 @@
-// Navigation bar items list
-
-export const navBar_Items = [
-    { label: "Home", href:"#"},
-    { label: "About Us", href:"#"},
-    { label: "Our Services", href:"#"},
-    { label: "Contact", href:"#"}
-];
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h1 className={`font-bold text-4xl`}>Total Textile Solutions</h1>
+  );
+}
